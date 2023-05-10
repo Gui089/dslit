5b56fe0a2aa2fdbd5d1eaa646e497a79cb83be94
Gui089 <guilhermesilba811@gmail.com>
2023-05-10 07:48:53 -0300

    propeties, game, ORM

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,115 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String plataform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+
+    }
+
+    public Game(Long id, String title, Integer year,String genre, String name,
+                String plataform, String imgUrl, String shortDescription, String longDescription){
+     super();
+     this.id = id;
+     this.genre = genre;
+     this.plataform = plataform;
+     this.imgUrl = imgUrl;
+     this.shortDescription = shortDescription;
+     this.longDescription = longDescription;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlataform() {
+        return plataform;
+    }
+
+    public void setPlataform(String plataform) {
+        this.plataform = plataform;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game game)) return false;
+        return getId().equals(game.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
