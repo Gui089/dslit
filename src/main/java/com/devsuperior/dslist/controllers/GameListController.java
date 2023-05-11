93634ecee44b8fc91aab7eee5555e6f6787bb297
Gui089 <guilhermesilba811@gmail.com>
2023-05-11 06:53:21 -0300

    endpoint lists

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameDto;
+import com.devsuperior.dslist.dto.GameListDto;
+import com.devsuperior.dslist.dto.GameMinDto;
+import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDto> findAll() {
+        List<GameListDto> result = gameListService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslist.services;
+
+
+import com.devsuperior.dslist.dto.GameDto;
+import com.devsuperior.dslist.dto.GameListDto;
+import com.devsuperior.dslist.dto.GameMinDto;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDto> findAll(){
+       List<GameList> result = gameListRepository.findAll();
+       return result.stream().map(x -> new GameListDto(x)).toList();
+    }
+}
